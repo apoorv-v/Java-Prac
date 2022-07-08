64fbe68e1fae881401c6036946d08b4ad4041d7c
apoorva.verma <apoorvaverma1999@gmail.com>
2022-07-08 16:12:18 +0530

    Init

---
+++ src/Car.java
@@ -0,0 +1,4 @@
+public class Car {
+    static String format = "SS-SS-NNNN";
+    String number = "UP16WALE";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.lang.reflect.Array;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static void func(){
+
     }
+
+    public static void main(String[] args) {
+        int a[] = {1,2,3};
+        printArr(a);
+    }
+
+    private static void printArr(int[] a) {
+        for (int el:
+                a) {
+            System.out.print(el + " ");
+        }
+    }
+
+
 }
